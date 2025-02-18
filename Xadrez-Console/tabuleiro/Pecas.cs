5363ebd8d2016947a576da3ef1b8d76ef389d38e
VictorConrado <estudantecomv@gmail.com>
2025-02-17 23:15:07 -0300

    Começando a implementar o tabuleiro.

---
+++ Xadrez-Console/tabuleiro/Pecas.cs
@@ -0,0 +1,19 @@
+﻿
+namespace tabuleiro
+{
+     class Pecas
+    {
+        public Posicao posicao {  get; set; }
+        public Cor cor {  get; set; }  
+        public int qtdeMovimentos {  get; set; }
+        public Tabuleiro tabuleiro { get; set; }
+
+        public Pecas(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tabuleiro = tabuleiro;
+            this.qtdeMovimentos = 0;
+        }
+    }
+}
---
+++ Xadrez-Console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,17 @@
+﻿
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Pecas[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            pecas = new Pecas[Linhas, Colunas];
+        }
+    }
+}
