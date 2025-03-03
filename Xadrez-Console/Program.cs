c85f585604c2272ebdf95492ba6750bd16c48ccf
VictorConrado <estudantecomv@gmail.com>
2025-03-03 20:44:08 -0300

    Criando a classe PosicaoXadrez

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http.Headers;
 using tabuleiro;
+using xadrez;
 using Xadrez_Console.xadrez;
 namespace Xadrez_Console
 {
@@ -7,20 +8,26 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            try { 
-            Tabuleiro tab = new Tabuleiro(8,8);
+            //try { 
+            //Tabuleiro tab = new Tabuleiro(8,8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(2, 4));
-            tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 7));
+            //tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            //tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(1, 3));
+            //tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(2, 4));
+            //tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 7));
 
-            Tela.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException ex)
-            {
-              Console.WriteLine(ex.Message);
-            }
+            //Tela.imprimirTabuleiro(tab);
+            //}
+            //catch (TabuleiroException ex)
+            //{
+            //  Console.WriteLine(ex.Message);
+            //}
+            //Console.ReadLine();
+
+            PosicaoXadrez pos = new PosicaoXadrez('c',7);
+
+            //Console.Write(pos);
+           Console.WriteLine(pos.toPosicao());
             Console.ReadLine();
         }
     }
